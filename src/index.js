d699904df164d9fc885a845fed52d0d358cef6e9
Bogdan <bogdanberezka@gmail.com>
2022-12-10 20:42:40 +0200

    add_simplelightbox

--- src/index.js
+++ src/index.js
@@ -1,22 +1,27 @@
-// import debounce from 'lodash.debounce';
-import Notiflix, { Notify } from 'notiflix';
-
-import PixabayApiService from './pixabay-service';
-
-// const API_KEY = '31955904-7341a4dddd0022ded7445126a';
-// const URL = `https://pixabay.com/api/?key=${API_KEY}&q=${name}&image_type=photo&orientation=horizontal&safesearch=true&page=1&per_page=40`;
+// import './css/styles.css';
+import { makeMarkup } from './js/make-markup';
+import PixabayApiService from './js/pixabay-service';
+import LoadMoreBtn from './js/load-more-btn';
+import { Notify } from 'notiflix';
+import SimpleLightbox from 'simplelightbox';
+import 'simplelightbox/dist/simple-lightbox.min.css';
 
 const refs = {
-  inputEl: document.querySelector('#search-form'),
-  showMoreBtnEl: document.querySelector('.show-more-btn'),
+  searchFormEl: document.querySelector('#search-form'),
   galleryEl: document.querySelector('.gallery'),
 };
 
+const loadMoreBtn = new LoadMoreBtn({
+  selector: '[data-action="load-more"]',
+  hidden: true,
+});
+
 const pixabayApiService = new PixabayApiService();
 
-refs.showMoreBtnEl.setAttribute('disabled', true);
-refs.inputEl.addEventListener('submit', onFormSubmit);
-refs.showMoreBtnEl.addEventListener('click', onLoadMore);
+const lightbox = new SimpleLightbox('.gallery a', { captionDelay: 250 });
+
+refs.searchFormEl.addEventListener('submit', onFormSubmit);
+loadMoreBtn.refs.button.addEventListener('click', onLoadMore);
 
 function onFormSubmit(evt) {
   evt.preventDefault();
@@ -24,42 +29,43 @@ function onFormSubmit(evt) {
   pixabayApiService.query = evt.currentTarget.elements.searchQuery.value.trim();
 
   if (pixabayApiService.query === '') {
-    Notify.failure('Введіть свій пошуковий запит');
-    return;
+    return Notify.info('Enter a search term');
   }
-
+  loadMoreBtn.show();
   pixabayApiService.resetPage();
-  pixabayApiService.fetchImages().then(appendDataMarkup);
+  onLoadMore();
 }
 
 function onLoadMore() {
-  pixabayApiService.fetchImages().then(appendDataMarkup);
+  loadMoreBtn.disable();
+  pixabayApiService
+    .fetchImages()
+    .then(data => {
+      if (data.total === 0) {
+        Notify.failure(
+          'Sorry, there are no images matching your search query. Please try again.'
+        );
+        loadMoreBtn.hide();
+        return;
+      }
+      addMarkup(data);
+      lightbox.refresh();
+
+      if (refs.galleryEl.children.length === data.totalHits) {
+        Notify.info(
+          `We're sorry, but you've reached the end of search results.`
+        );
+        loadMoreBtn.hide();
+      } else {
+        loadMoreBtn.enable();
+        Notify.success(`Hooray! We found ${data.totalHits} images.`);
+      }
+    })
+    .catch(error => console.log(error));
 }
 
-function appendDataMarkup(items) {
-  const dataListMarkup = items
-    .map(item => {
-      return `<div class="photo-card">
-      <img src="${item.previewURL}" alt=${item.tags}" loading="lazy" />
-      <div class="info">
-        <p class="info-item">
-          <b>Likes ${item.likes}</b>
-        </p>
-        <p class="info-item">
-          <b>Views ${item.views}</b>
-        </p>
-        <p class="info-item">
-          <b>Comments ${item.comments}</b>
-        </p>
-        <p class="info-item">
-          <b>Downloads ${item.downloads}</b>
-        </p>
-      </div>
-    </div>`;
-    })
-    .join(' ');
-  refs.galleryEl.insertAdjacentHTML('beforeend', dataListMarkup);
-  refs.showMoreBtnEl.removeAttribute('disabled');
+function addMarkup(hits) {
+  refs.galleryEl.insertAdjacentHTML('beforeend', makeMarkup(hits));
 }
 
 function clearHtmlMarkup() {
